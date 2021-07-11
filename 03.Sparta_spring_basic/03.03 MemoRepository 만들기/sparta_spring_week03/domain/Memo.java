5ecc222393823b5fcbf65b140ee761d2cb7c593f
xhaktmchl <62229967+xhaktmchl@users.noreply.github.com>
2021-07-12 01:12:39 +0900

    Add files via upload

---
+++ 03.Sparta_spring_basic/03.03 MemoRepository 만들기/sparta_spring_week03/domain/Memo.java
@@ -0,0 +1,40 @@
+package com.sparta_spring.sparta_spring_week03.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor // 빈생성자
+@Getter
+@Entity // 테이블과 연계됨을 스프링에게 알려줍니다.
+public class Memo extends Timestamped{
+
+    // 아이디
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    // 열
+    // 사용자 이름
+    @Column(nullable = false)
+    private String username;
+    // 메모 내용
+    @Column(nullable = false)
+    private String contents;
+
+    // 생성자
+    // 유저이름, 메모내용으로 생성자
+    public Memo(String username,String contents){
+        this.username = username;
+        this.contents = contents;
+    }
+
+    // DTO를 이용해서 생성
+    public Memo(MemoRequestDto requestDto){
+        this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
+    }
+
+
+}
---
+++ 03.Sparta_spring_basic/03.03 MemoRepository 만들기/sparta_spring_week03/domain/Timestamped.java
@@ -0,0 +1,21 @@
+package com.sparta_spring.sparta_spring_week03.domain;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass // Entity가 자동으로 컬럼으로 인식합니다.
+@EntityListeners(AuditingEntityListener.class) // 생성/변경 시간을 자동으로 업데이트합니다.
+public abstract class Timestamped {//abstract 추상형: new로 생성 못함
+
+    @CreatedDate // 생성날짜
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate// 마지막 수정낳=ㄹ짜
+    private LocalDateTime modifiedAt;
+
+}
