d06d5b53951c3a8b315118fdb6d744089d3a6123
xhaktmchl <62229967+xhaktmchl@users.noreply.github.com>
2021-07-15 19:01:19 +0900

    관심 상품 등록하기 코드 업로드

---
+++ 03.Sparta_spring_basic/04.07 관심 상품 등록하기/sparta_spring_week04/models/Product.java
@@ -0,0 +1,52 @@
+package com.sparta_spring.sparta_spring_week04.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity // DB 테이블 역할을 합니다.
+@Getter // get 함수를 일괄적으로 만들어줍니다.
+@NoArgsConstructor // 빈 생성자
+public class Product extends Timestamped{
+
+    // ID
+    @GeneratedValue(strategy = GenerationType.AUTO) // 자동생성
+    @Id
+    private Long id;
+
+    // 열
+    // 상품 이름
+    @Column(nullable = false)
+    private String title;
+
+    // 상품 사진
+    @Column(nullable = false)
+    private String img;
+
+    // 상품 링크
+    @Column(nullable = false)
+    private String link;
+
+    // 최저가
+    @Column(nullable = false)
+    private int lprice;
+
+    // 내 상품 가격
+    @Column(nullable = false)
+    private int myprice;
+
+    // 관심상품 등록시 필요한 생성자
+    public Product(ProductRequestDto requestDto){
+        this.title = requestDto.getTitle();
+        this.img = requestDto.getImg();
+        this.link = requestDto.getLink();
+        this.lprice = requestDto.getLprice();
+        this.myprice =0; // 내가 등록한 가격
+    }
+
+    // 관심상품의 최저가격 업데이트 메서드
+    public void update(ProductMypriceRequestDto requestDto){
+        this.myprice = requestDto.getMyprice();
+    }
+}
---
+++ 03.Sparta_spring_basic/04.07 관심 상품 등록하기/sparta_spring_week04/models/ProductRequestDto.java
@@ -0,0 +1,18 @@
+package com.sparta_spring.sparta_spring_week04.models;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+// 정보를 전달하는 매개체이므로 정보를 담고 생성,조회기능있어야 됨
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class ProductRequestDto { // 관심상품 등록정보 매개체
+
+    private String title; // 상품이름
+    private String img; // 상품사진
+    private int lprice; // 상품 최저가
+    private String link; // 상품 링크크
+
+}
