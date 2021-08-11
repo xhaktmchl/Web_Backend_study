db883f2a6a8f245a203e5c0ce6211c4a36077b0b
xhaktmchl <62229967+xhaktmchl@users.noreply.github.com>
2021-08-11 22:29:07 +0900

    추가된 부분만 업로드

---
+++ 04.Sparta_spring_core/01.06. 나만의 셀렉샵 API 구현하기/java/com/sparta_spring/sparta_springcore_week01/SpartaSpringcoreWeek01Application.java
@@ -0,0 +1,15 @@
+package com.sparta_spring.sparta_springcore_week01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing // 시간 자동 변경이 가능하도록 합니다.
+@ServletComponentScan
+public class SpartaSpringcoreWeek01Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaSpringcoreWeek01Application.class, args);
+    }
+}
